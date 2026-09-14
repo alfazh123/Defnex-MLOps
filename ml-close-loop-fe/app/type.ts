c8bbb987c1f30ef1a9b4aa7295755e3e355defa1
alfazh123 <alfazh291@gmail.com>
2026-09-14 11:00:44 +0700

    fix data type

--- ml-close-loop-fe/app/type.ts
+++ ml-close-loop-fe/app/type.ts
@@ -13,12 +13,45 @@ export type Dataset = {
 }
 
 export type TrainingRun = {
-    runId: string;
-    targetModel: string;
-    targetModelBase: string;
-    datasetVersion: string;
-    status: "COMPLETED" | "RUNNING" | "PENDING" | "FAILED";
-    trainLoss: number | null;
-    evalLoss: number | null;
-    created: Date;
-}
+	runId: string;
+	targetModel: string;
+	targetModelBase: string;
+	datasetVersion: string;
+	status: "COMPLETED" | "RUNNING" | "PENDING" | "FAILED";
+	trainLoss: number | null;
+	evalLoss: number | null;
+	created: Date;
+	loraConfig: {
+		lr: number;
+		epochs: number;
+		maxSeqLength: number;
+		batch: number;
+		loraRank: number;
+		loraAlpha: number;
+	};
+	checkpointPath: string;
+};
+
+export type TargetSchemaProp = {
+	id: string;
+	title: string;
+	version: string;
+	description: string;
+	requiredFields: number;
+	optionalFields: number;
+};
+
+export type ValidationCheck = {
+	id: string;
+	title: string;
+	status: "success" | "failed" | "pending";
+};
+
+export type RecordSample = {
+	id: string;
+	screnarioId: string;
+	domain: string;
+	environment: string;
+	time: string;
+	location: string;
+};
